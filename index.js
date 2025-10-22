9f6e1740935103e4d4ee4d52253ee9e8f3f6050c
Ananda Agung Ismail <158981471+CaptainAAI@users.noreply.github.com>
2025-10-22 10:57:39 +0700

    membuat mySQL connection

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+let mysql = require('mysql');
+const app = express();
+const PORT = 3001;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  port: '3306',
+  password: '',
+  database: 'mahasiswa'
+});
